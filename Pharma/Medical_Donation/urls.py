d933003b6d381a4513b4d808543f4ab61932e92a
shubham2637 <shubham37@outlook.com>
2018-10-21 17:36:37 +0530

    Database Connection done

--- Pharma/Medical_Donation/urls.py
+++ Pharma/Medical_Donation/urls.py
@@ -5,8 +5,9 @@ path("", views.index, name="index"),
 path("collector", views.collectors, name= "Collector_view"),
 path("donor", views.donors, name= "Donor_view"),
 path("add-collector", views.collector_add, name="Add_Collector"),
-path("collector_form", views.Collector_form, name="form_name"),
+path("collector_form", views.collector_form_view, name="form_name"),
 path("add-donor", views.donor_add, name="add_donor"),
-path("create", views.create)
+path("create_Collector", views.create_Collector),
+path("create_Doner", views.create_Doner)
 
 ]
--- Pharma/Medical_Donation/views.py
+++ Pharma/Medical_Donation/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.urls import reverse
 # Create your views here.
 from .models import Collector, Doner
-from .forms import *
+from . import forms
 
 
 def index(request):
@@ -29,13 +29,13 @@ def donors(request):
 
 def form_Collector(request):
     form = forms.Collector_form()
-    if request.method:
+    if request.method == POST:
         form = forms.Collector_form(request.POST)
     context={
     "form" : form
     }
 
-    return render(request, "Medical_Donation/collector_form.html")
+    return render(request, "Medical_Donation/collector_form.html", context)
 
 def collector_add(request):
     context = {
@@ -54,13 +54,30 @@ def donor_add(request):
     return render(request, "Medical_Donation/add-donor.html",context)
 
 
-def create(request):
+def create_Collector(request):
     if request.POST:
-        if request.POST['pincode']:
-            pin = request.POST['pincode']
-        else:
-            pin = False
-        coll = Collector(name=request.POST['name'], address=(request.POST['address1']+ request.POST['address2'] + request.POST['address3']), pinCode=pin, Phone_no=request.POST['phone_no'],
-                BirthDate=request.POST['birthdate'], UID=request.POST['uid'], email=request.POST['email'],username=request.POST['username'], password=request.POST['password'], image=request.POST['image'] )
-        coll.save()
+            coll = Collector(name=request.POST['name'], address= (request.POST['address1']+ request.POST['address2'] + request.POST['address3']), pinCode=request.POST['pincode'], Phone_no=request.POST['phone_no'],BirthDate=request.POST['birth'], UID=request.POST['uid'], email=request.POST['email'],username=request.POST['username'], password=request.POST['password'], image=request.POST['image'] )
+            coll.save()
     return HttpResponseRedirect(reverse("Add_Collector"))
+
+
+def create_Doner(request):
+    if request.POST:
+            don = Doner(name=request.POST['name'], address= (request.POST['address1']+ request.POST['address2'] + request.POST['address3']), pinCode=request.POST['pincode'], Phone_no=request.POST['phone_no'],BirthDate=request.POST['birth'], UID=request.POST['uid'], email=request.POST['email'],username=request.POST['username'], password=request.POST['password'], image=request.POST['image'] )
+            don.save()
+    return HttpResponseRedirect(reverse("add_donor"))
+
+
+def collector_form_view(request):
+    if request.method == "POST":
+        form = forms.Collector_form(request.POST)
+        if form.is_valid():
+                item = form.save(commit=False)
+                item.save()
+    else:
+        form = forms.Collector_form()
+
+    context={
+    "form" : form
+    }
+    return render(request, "Medical_Donation/collector_form.html", context)
